3d7ac32a845ec610aadacfb7808bb8eae1a988e2
yusuf ozturk <yusuf@MacBook-Pro-von-yusuf.local>
2022-06-06 17:18:38 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div >
+      Wallet App
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client'
+import App from './App';
+import 'antd/dist/antd.css'
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <App />
+);
+
